b71db3a4517171ce97a773f88619508ad1912e5e
R Wood <archaurore@gmail.com>
2019-05-19 10:33:50 +0200

    added a user list

--- routes/static.js
+++ routes/static.js
@@ -3,8 +3,19 @@
 const express = require('express');
 const router = express.Router();
 
+const users = [
+    { name : 'John Doe' }
+];
+
 router.get('/', (req, res) => {
     return res.render('index', { title : 'User Form' });
+});
+
+router.get('/users', (req, res) => {
+    return res.render('user-list', {
+        title : 'User List',
+        users : users
+    })
 });
 
 module.exports = router;
