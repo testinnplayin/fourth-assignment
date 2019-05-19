d8bdf18ab64e3cf135e0bf74f704472a2223c17a
R Wood <archaurore@gmail.com>
2019-05-19 10:15:03 +0200

    created basic index view

--- server.js
+++ server.js
@@ -3,7 +3,14 @@
 const express = require('express');
 const app = express();
 
+const staticRoutes = require('./routes/static');
+
 const PORT = 3000;
+
+app.set('view engine', 'pug');
+app.set('views', 'views'); // reiterated here so that I don't forget about it even though it's set by default
+
+app.use('/', staticRoutes);
 
 app.use('*', (req, res) => {
     return res.status(404).json({ error : 'Page not found' });
